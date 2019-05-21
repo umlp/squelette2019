87263831d5da4226de807b3cf419f858de4d7136
BAUCHER Frederic <frederic.baucher@cegedim.fr>
2019-05-21 20:12:31 +0200

    operationn CRUD sur IHM a partir de https://o7planning.org/fr/11545/tutoriel-spring-boot-et-thymeleaf#

---
+++ src/main/java/com/hellokoding/ihm/PersonForm.java
@@ -0,0 +1,25 @@
+package com.hellokoding.ihm;
+  
+// classe de l'IHM : abstraction d'un formulaire 
+public class PersonForm {
+ 
+    private String firstName;
+    private String lastName;
+ 
+    public String getFirstName() {
+        return firstName;
+    }
+ 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+ 
+    public String getLastName() {
+        return lastName;
+    }
+ 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+     
+}
